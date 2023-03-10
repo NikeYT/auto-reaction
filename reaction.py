4dab92058127d138536b6c33bd0f17771b9c202d
Timur <timurkaratikeev68@gmail.com>
2023-03-10 21:10:15 +0500

    Create reaction.py

---
+++ reaction.py
@@ -0,0 +1,27 @@
+from pyrogram import Client
+from pyrogram import types, filters
+from pyrogram.errors.exceptions.bad_request_400 import ReactionInvalid, ReactionEmpty
+from random import choice
+
+# Инициализация клиента
+app = Client(
+    api_id='---',
+    api_hash='---'
+)
+
+@app.on_message(filters=filters.chat("---"))
+def call_react(client: Client, message: types.Message):
+    smile = '👎' #Change to your smile
+    try:
+        app.send_message(chat_id=message.chat.id, message_id=message.id, emoji=choice(smile))
+    except (ReactionInvalid, ReactionEmpty):
+        for i in smile:
+            try:
+                app.send_reaction(chat_id=message.chat.id, message_id=message.id, emoji=i)
+                break
+            except ReactionInvalid:
+                continue
+
+if __name__ == "__main__":
+    print('Script started')
+    app.run()
